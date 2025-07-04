d79f9ebfdda983cf768838d0bc793877a25052ec
orang biasa <68330655+normalycreate@users.noreply.github.com>
2025-07-04 14:24:57 +0700

    Update 2-3-04-07-25.js

--- 3-03-07-25/script.js
+++ 3-03-07-25/script.js
@@ -2,30 +2,30 @@ const i1 = "kertas"; const i2 = "batu"; const i3 = "gunting";
 const o1 = "menang"; const o2 = "setara"; const o3 = "kalah";
 document.getElementById("gunting").addEventListener("click", function(){
     if (fungsiacak() === "gunting") {
-        templatemsg() = `Anda memilih ${i3} dan komputer memilih ${i3} maka anda ${o2}`;
+        templatemsg(`Anda memilih ${i3} dan komputer memilih ${i3} maka anda ${o2}`);
     } else if (fungsiacak() === "batu") {
-        templatemsg() = `Anda memilih ${i3} dan komputer memilih ${i2} maka anda ${o3}`;
+        templatemsg(`Anda memilih ${i3} dan komputer memilih ${i2} maka anda ${o3}`);
     } else {
-        templatemsg() = `Anda memilih ${i3} dan komputer memilih ${i1} maka anda ${o1}`
+        templatemsg(`Anda memilih ${i3} dan komputer memilih ${i1} maka anda ${o1}`);
     }
 });
 document.getElementById("batu").addEventListener("click", function(){
     if (fungsiacak() === "gunting") {
-        templatemsg() = `Anda memilih ${i2} dan komputer memilih ${i3} maka anda ${o1}`
+        templatemsg(`Anda memilih ${i2} dan komputer memilih ${i3} maka anda ${o1}`);
     } else if (fungsiacak() === "batu") {
-        templatemsg() = `Anda memilih ${i2} dan komputer memilih ${i2} maka anda ${o2}`
+        templatemsg(`Anda memilih ${i2} dan komputer memilih ${i2} maka anda ${o2}`);
     } else {
-        templatemsg() = `Anda memilih ${i2} dan komputer memilih ${i1} maka anda ${o3}`
+        templatemsg(`Anda memilih ${i2} dan komputer memilih ${i1} maka anda ${o3}`);
     }    
 });
 document.getElementById("kertas").addEventListener("click", function(){
     fungsiacak();
     if (fungsiacak() === "gunting") {
-        templatemsg() = `Anda memilih ${i1} dan komputer memilih ${i3} maka anda ${o3}`
+        templatemsg(`Anda memilih ${i1} dan komputer memilih ${i3} maka anda ${o3}`);
     } else if (fungsiacak() === "batu") {
-        templatemsg() = `Anda memilih ${i1} dan komputer memilih ${i2} maka anda ${o1}`
+        templatemsg(`Anda memilih ${i1} dan komputer memilih ${i2} maka anda ${o1}`);
     } else {
-        templatemsg() = `Anda memilih ${i1} dan komputer memilih ${i1} maka anda ${o2}`
+        templatemsg(`Anda memilih ${i1} dan komputer memilih ${i1} maka anda ${o2}`);
     }    
 });
 function fungsiacak() {
@@ -34,6 +34,6 @@ function fungsiacak() {
     let pilihanKomputer = pilihan[acak];   
     return pilihanKomputer;
 }
-function templatemsg() {
-    document.getElementById("hasil").innerHTML
+function templatemsg(msg) {
+    document.getElementById("hasil").innerHTML = msg;
 }
