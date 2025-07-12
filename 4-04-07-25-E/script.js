391cbc0acd1e2f40567e57c2d4111c2073585227
orang biasa <68330655+normalycreate@users.noreply.github.com>
2025-07-12 16:32:01 +0700

    Update script.js

--- 4-04-07-25-E/script.js
+++ 4-04-07-25-E/script.js
@@ -1,10 +1,21 @@
-const whatDYD = [];
+console.log("Kode javascript berhasil dijalakankan");
 
-function addTask() {
-    const whatDoInput = document.getElementById('whatForList');
-    const doing = whatDoInput.value;
-
-    whatDYD.push(doing);
-    console.log(whatDYD);
-    whatDoInput.value = null;
-};           
+const inputList = document.querySelector(".writeForList");
+const submitWrite = document.getElementById("sendToDo");
+const addList = document.querySelector(".displayedList");
+document.getElementById("sendToDo").addEventListener("click", function() {
+    const takeInput = inputList.value; //I want take input value from writeToDo
+    //I want to validate the input if the input is empty it will be displayed the error message
+    if (takeInput.trim() === "") {
+        console.error("Input Kosong")
+        alert("Input Kosong dan anda tidak memasukan input apapun!")
+        return // biar fungsi beneran berhenti dan ga ada baris kosong
+    } 
+    //after that i want to add new HTML element in the HTML 
+    const addNewLine = document.createElement('p'); // creating element base on inputList
+    addNewLine.textContent = takeInput;
+    addNewLine.classList.add('addNewListView'); // adding new classes in element
+    addList.appendChild(addNewLine);
+    //empty the input list again to fill in the next time
+    inputList.value = "";
+})       
